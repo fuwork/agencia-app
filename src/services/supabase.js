0d4ccf8279695b39e7c3b0402c7d5f4ab8cae615
airtonsouza <airkun.2014@gmail.com>
2025-03-31 20:06:57 -0300

    segurança credencial supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const supabaseUrl = 'https://uvhymqggrunmuxgudljh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2aHltcWdncnVubXV4Z3VkbGpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1NjMyMTIsImV4cCI6MjA1NzEzOTIxMn0.suyRqurwzfrdtqeiXH5nRyxGvPb2alHC7LBIDbv5yaA';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
